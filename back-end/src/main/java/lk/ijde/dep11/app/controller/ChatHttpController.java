161fe956915ef85467b7d90a826c950d224ff319
Danidu Bogoda <danidubogoda95@gmail.com>
2023-12-17 17:10:07 +0530

    Setup HttpController

--- back-end/src/main/java/lk/ijde/dep11/app/controller/ChatHttpController.java
+++ back-end/src/main/java/lk/ijde/dep11/app/controller/ChatHttpController.java
@@ -1,9 +1,16 @@
 package lk.ijde.dep11.app.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 @RestController
 @RequestMapping("/api/v1/messages")
@@ -11,5 +18,30 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class ChatHttpController {
 
+    private List<String> chatMessages = new Vector<>();
+
+    public void validationExceptionHandler(ConstraintViolationException exp){
+        ResponseStatusException resExp = new ResponseStatusException(HttpStatus.BAD_REQUEST, exp.getMessage());
+
+        exp.initCause(resExp);
+        throw resExp;
+
+    }
+
+    @GetMapping(produces = "application/json")
+    public List<String> retrieveMessages(){
+        return chatMessages;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    @Validated
+    public Map<String, String> sendMessage(@RequestBody Map<
+                    @Pattern(regexp = "^messgge$", message = "Invalid chat messages")String,
+                    @NotBlank(message = "Chat messages can't be empty")String>
+                                           messageObj){
+        chatMessages.add(messageObj.get("message"));
+        return messageObj;
+    }
 
 }
--- back-end/src/main/java/lk/ijde/dep11/app/to/MessageTo.java
+++ back-end/src/main/java/lk/ijde/dep11/app/to/MessageTo.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MessageTo {
 
+    @NotBlank(message = "Chat message can't be empty")
     private String message;
 
+    @NotBlank(message = "Email can't be empty")
+    @Email(message = "Invalid Email")
     private String email;
 
 }
